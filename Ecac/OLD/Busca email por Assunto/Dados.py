0cdc60d25e53f141f1dce6e6e8f4618497020ca8
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2022-08-30 14:18:18 -0300

    Delete Ecac directory

--- Ecac/OLD/Busca email por Assunto/Dados.py
+++
@@ -1,5 +0,0 @@
-empresas = [
-("21854407000156", r"..\certificados\CERT RPEM 35586086.pfx", "35586086"),
-("31203627000173", r"..\certificados\CERT RPEM 35586086.pfx", "35586086"),
-("10769759000197", r"..\certificados\CERT RPEM 35586086.pfx", "35586086"),
-]

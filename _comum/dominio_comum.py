a24d9b5ce0fc38d8a527932840a1dd471241b306
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2022-09-26 15:44:47 -0300

    backup 26-09-2022

--- _comum/dominio_comum.py
+++ _comum/dominio_comum.py
@@ -2,22 +2,26 @@
 import pyperclip, time, os, pyautogui as p
 from datetime import datetime
 from sys import path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 path.append(r'..\..\_comum')
 from comum_comum import _escreve_relatorio_csv
+from chrome_comum import _initialize_chrome, _send_input
 
 
 def _login(empresa, andamentos):
     cod, cnpj, nome = empresa
     # espera a tela inicial do domínio
-    while p.locateOnScreen(r'imgs_c\inicial.png', confidence=0.9):
-        time.sleep(1)
+    '''while p.locateOnScreen(r'imgs_c\inicial.png', confidence=0.5):
+                    time.sleep(1)'''
+
+    p.click(833,384)
 
     # espera abrir a janela de seleção de empresa
     while not p.locateOnScreen(r'imgs_c\trocar_empresa.png', confidence=0.9):
         p.press('f8')
-    while p.locateOnScreen(r'imgs_c\trocar_empresa.png', confidence=0.9):
-        time.sleep(1)
     
     # clica para pesquisar empresa por código
     if p.locateOnScreen(r'imgs_c\codigo.png', confidence=0.9):
@@ -68,3 +72,61 @@ def _login(empresa, andamentos):
     p.press('esc', presses=5)
 
     return True
+
+
+def _login_web():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1920,1080')
+    
+    status, driver = _initialize_chrome()
+    
+    driver.get('https://www.dominioweb.com.br/')
+    _send_input('/html/body/app-root/app-login/div/div/fieldset/div/div/section/form/label[1]/span[2]/input', 'robo@veigaepostal.com.br', driver)
+    _send_input('/html/body/app-root/app-login/div/div/fieldset/div/div/section/form/label[2]/span[2]/input', 'Rb#0086*', driver)
+    driver.find_element(by=By.ID, value='enterButton').click()
+    
+    caminho = os.path.join('imgs_c', 'abrir_app.png')
+    caminho2 = os.path.join('imgs_c', 'abrir_app_2.png')
+    while not p.locateOnScreen(caminho, confidence=0.9):
+        if p.locateOnScreen(caminho2, confidence=0.9):
+            sleep(1)
+            while p.locateOnScreen(caminho2, confidence=0.9):
+                p.click(p.locateCenterOnScreen(caminho2, confidence=0.9), button='left')
+                return driver
+        
+        sleep(1)
+    if p.locateOnScreen(caminho, confidence=0.9):
+        p.click(p.locateCenterOnScreen(caminho, confidence=0.9), button='left')
+        return driver
+
+
+def _abrir_escrita_fiscal(driver):
+    while not p.locateOnScreen(r'imgs_c/modulos.png', confidence=0.9):
+        sleep(1)
+        try:
+            p.getWindowsWithTitle('Lista de Programas')[0].activate()
+        except:
+            pass
+    sleep(3)
+    driver.quit()
+    p.click(p.locateCenterOnScreen(r'imgs_c/escrita_fiscal.png', confidence=0.9), button='left', clicks=2)
+    while not p.locateOnScreen(r'imgs_c/login_modulo.png', confidence=0.9):
+        sleep(1)
+    
+    p.moveTo(p.locateCenterOnScreen(r'imgs_c/insere_usuario.png', confidence=0.9))
+    local_mouse = p.position()
+    p.click(int(local_mouse[0] + 120), local_mouse[1], clicks=2)
+    
+    sleep(0.5)
+    p.press('del', presses=10)
+    p.write('robo')
+    sleep(0.5)
+    p.press('tab')
+    sleep(0.5)
+    p.press('del', presses=10)
+    p.write('Rb#0086*')
+    sleep(0.5)
+    p.hotkey('alt', 'o')
+    while not p.locateOnScreen(r'imgs_c/onvio.png', confidence=0.9):
+        sleep(1)

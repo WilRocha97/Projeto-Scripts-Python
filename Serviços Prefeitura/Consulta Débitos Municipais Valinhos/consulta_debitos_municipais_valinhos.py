916a8bc8b9347d6ce38114a32c955aede2166c00
WilRocha97 <willrbr1997@gmail.com>
2023-01-04 13:48:59 -0300

    backup 04-01-2023

--- Serviços Prefeitura/Consulta Débitos Municipais Valinhos/consulta_debitos_municipais_valinhos.py
+++ Serviços Prefeitura/Consulta Débitos Municipais Valinhos/consulta_debitos_municipais_valinhos.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # from selenium.common.exceptions import NoSuchElementException
+import re
+
 from bs4 import BeautifulSoup
 from xhtml2pdf import pisa
 from selenium import webdriver
@@ -109,6 +111,7 @@ def login(options, cnpj, insc_muni):
 
     if not captcha:
         print('Erro Login - não encontrou captcha')
+        return 'erro captcha'
 
     if captcha is not None:
         while not find_by_id('input1', driver):
@@ -131,41 +134,55 @@ def login(options, cnpj, insc_muni):
         button = driver.find_element(by=By.ID, value='imagebutton1')
         button.click()
 
-        time.sleep(2)
-
-        if find_by_id('tdMsg', driver):
+        time.sleep(3)
+        
+        print('>>> Aguardando site')
+        timer = 0
+        while not find_by_id('td30', driver):
             driver.save_screenshot(r'ignore\debug_screen.png')
-            _escreve_relatorio_csv(';'.join([cnpj, 'Erro no login']))
-            print('❌ Erro no login')
-        else:
-            while not find_by_id('tdTitulo', driver):
-                time.sleep(1)
-            html = driver.page_source.encode('utf-8')
-            try:
-                driver.save_screenshot(r'ignore\debug_screen.png')
-                str_html = format_data(html)
+            time.sleep(1)
 
-                if str_html:
-                    _escreve_relatorio_csv(';'.join([cnpj, 'Com débitos']))
-                    print('>>> Gerando arquivo')
-                    nome_arq = ';'.join([cnpj, 'INF_FISC_REAL', 'Debitos Municipais'])
-                    with open('execução/documentos/' + nome_arq + r'.pdf', 'w+b') as pdf:
-                        pisa.showLogging()
-                        pisa.CreatePDF(str_html, pdf)
-                        print('❗ Arquivo gerado')
-                else:
-                    _escreve_relatorio_csv(';'.join([cnpj, 'Sem débitos']))
-                    print('✔ Não há débitos')
-            except:
-                _escreve_relatorio_csv(';'.join([cnpj, 'Erro na geração do PDF']))
-                driver.save_screenshot(r'ignore\debug_screen.png')
-                print('❌ Erro na geração do PDF')
+            if find_by_id('tdMsg', driver):
+                try:
+                    erro = re.compile(r'\"erroMessageText\".+tipo=\"td\">(.+)</td></tr></tbody></table>').search(driver.page_source).group(1)
+                    if erro != 'td':
+                        _escreve_relatorio_csv(f'{cnpj};{erro}')
+                        print(f'❌ {erro}')
+                        return False
+                except:
+                    pass
+            timer += 1
+            if timer >= 10:
+                _escreve_relatorio_csv(f'{cnpj};Erro no login')
+                print(f'❌ Erro no login')
+                return False
+            
+        html = driver.page_source.encode('utf-8')
+        try:
+            driver.save_screenshot(r'ignore\debug_screen.png')
+            str_html = format_data(html)
+
+            if str_html:
+                _escreve_relatorio_csv(';'.join([cnpj, 'Com débitos']))
+                print('>>> Gerando arquivo')
+                nome_arq = ';'.join([cnpj, 'INF_FISC_REAL', 'Debitos Municipais'])
+                with open('execução/documentos/' + nome_arq + r'.pdf', 'w+b') as pdf:
+                    pisa.showLogging()
+                    pisa.CreatePDF(str_html, pdf)
+                    print('❗ Arquivo gerado')
+            else:
+                _escreve_relatorio_csv(';'.join([cnpj, 'Sem débitos']))
+                print('✔ Não há débitos')
+        except:
+            _escreve_relatorio_csv(';'.join([cnpj, 'Erro na geração do PDF']))
+            driver.save_screenshot(r'ignore\debug_screen.png')
+            print('❌ Erro na geração do PDF')
 
         driver.close()
         return True
     else:
-        _escreve_relatorio_csv(';'.join([cnpj, 'Erro no captcha']))
-        print('❌ Erro no captcha')
+        _escreve_relatorio_csv(f'{cnpj};Erro no login')
+        print('❌ Erro no login')
         return True
 
 
@@ -187,8 +204,9 @@ def run():
 
         _indice(count, total_empresas, empresa)
 
-        if not login(options, cnpj, insc_muni):
-            break
+        resultado = 'erro captcha'
+        while resultado == 'erro captcha':
+            resultado = login(options, cnpj, insc_muni)
 
 
 if __name__ == '__main__':

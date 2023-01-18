65e5922eeb87225541f6c01c7395384b328af6df
WilRocha97 <willrbr1997@gmail.com>
2023-01-18 15:28:17 -0300

    backup 18-01-2023

--- Domínio/Gera Arquivo e Relatório DIRF/gera_arquivo_relatorio_dirf.py
+++ Domínio/Gera Arquivo e Relatório DIRF/gera_arquivo_relatorio_dirf.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import pyperclip, time, os, pyautogui as p
+import pyperclip, time, os, shutil, pyautogui as p
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from sys import path
@@ -7,7 +7,7 @@ from sys import path
 path.append(r'..\..\_comum')
 from pyautogui_comum import _find_img, _click_img, _wait_img
 from comum_comum import _indice, _time_execution, _escreve_relatorio_csv, e_dir, _open_lista_dados, _where_to_start
-from dominio_comum import _login, _salvar_pdf
+from dominio_comum import _login
 
 
 def dirf(empresa, ano, andamento):
@@ -39,17 +39,17 @@ def dirf(empresa, ano, andamento):
     p.write('1')
     time.sleep(0.5)
     p.press('tab')
-
+    
+    arquivos_dirf(empresa, ano, andamento)
+    
     if not relatorio_dirf(empresa, ano, andamento):
         return False
     
     
 def relatorio_dirf(empresa, ano, andamento):
-    cod, cnpj, nome = empresa
-    
     # seleciona para gerar o relatório
-    if _find_img('formulario.png'):
-        _click_img('formulario.png')
+    if _find_img('formulario.png', conf=0.9):
+        _click_img('formulario.png', conf=0.9)
     time.sleep(1)
     
     # seleciona para gerar com folha de pagamento
@@ -57,8 +57,8 @@ def relatorio_dirf(empresa, ano, andamento):
         _click_img('folha_de_pagamento.png')
     time.sleep(1)
     
-    # aabre a janela de outros dados
-    p.hotkey('alt', 'o')
+    # abre a janela de outros dados
+    p.hotkey('alt', 'u')
     
     while not _find_img('outros_dados.png', conf=0.9):
         time.sleep(1)
@@ -70,7 +70,7 @@ def relatorio_dirf(empresa, ano, andamento):
         time.sleep(1)
         p.press('esc')
         
-        _escreve_relatorio_csv(';'.join([cod, cnpj, nome, 'Não é possível editar aba de folha de pagamento']), nome=andamento)
+        _escreve_relatorio_csv('Não é possível editar aba de folha de pagamento', nome=andamento)
         print('❌ Não é possível editar aba de folha de pagamento')
         return False
     
@@ -82,12 +82,12 @@ def relatorio_dirf(empresa, ano, andamento):
         _click_img('todos.png', conf=0.95)
     time.sleep(1)
     
-    if _find_img('gerar_info_complementar.png', conf= 0.95):
-        _click_img('gerar_info_complementar.png', conf=0.95)
+    if _find_img('gerar_info_complementar.png', conf= 0.99):
+        _click_img('gerar_info_complementar.png', conf=0.99)
     time.sleep(1)
     
-    if _find_img('limitar_60_caractere.png', conf=0.95):
-        _click_img('limitar_60_caractere.png', conf=0.95)
+    if _find_img('limitar_60_caractere.png', conf=0.99):
+        _click_img('limitar_60_caractere.png', conf=0.99)
     time.sleep(1)
     
     p.hotkey('alt', 'g')
@@ -97,19 +97,139 @@ def relatorio_dirf(empresa, ano, andamento):
 
     p.hotkey('alt', 'o')
     
+    while not _find_img('relatorio_gerado.png', conf=0.9):
+        time.sleep(1)
+        
+    salvar_pdf(empresa)
     
-    # under construction
-    
-    
-    _salvar_pdf()
-    
-    _escreve_relatorio_csv(';'.join([cod, cnpj, nome, 'Arquivo gerado']), nome=andamento)
-    print('✔ Arquivo gerado')
+    _escreve_relatorio_csv(f'Relatório DIRF {ano} gerado', nome=andamento)
+    print('✔ Relatório gerado')
 
     p.press('esc', presses=4)
     time.sleep(2)
+    return True
 
 
+def arquivos_dirf(empresa, ano, andamento):
+    cod, cnpj, nome = empresa
+    
+    # seleciona para gerar o relatório
+    if _find_img('arquivo.png', conf=0.9):
+        _click_img('arquivo.png', conf=0.9)
+    time.sleep(1)
+    
+    # desce para a linha onde sera digitado o caminho para salvar o arquivo
+    p.press('tab')
+    time.sleep(1)
+    
+    # apaga qualuer texto que esteja no campo
+    p.press('del', presses=85)
+    
+    # digita o caminho para salvar o arquivo
+    os.makedirs('execução/Arquivos', exist_ok=True)
+    pyperclip.copy('V:\Setor Robô\Scripts Python\Domínio\Gera Arquivo e Relatório DIRF')
+    pyperclip.copy('V:\Setor Robô\Scripts Python\Domínio\Gera Arquivo e Relatório DIRF')
+    p.hotkey('ctrl', 'v')
+    
+    # abre a janela de outros dados
+    p.hotkey('alt', 'u')
+
+    while not _find_img('outros_dados.png', conf=0.9):
+        time.sleep(1)
+
+    p.hotkey('alt', 'g')
+
+    while not _find_img('dirf.png', conf=0.9):
+        time.sleep(1)
+
+    p.hotkey('alt', 'o')
+    
+    while not _find_img('dirf_gerada.png', conf=0.9):
+        time.sleep(1)
+        
+        if _find_img('sem_dados_arquivo.png', conf=0.9):
+            p.press('enter')
+            _escreve_relatorio_csv(f'{cod};{cnpj};{nome};Sem dados para emitir', nome=andamento, end=';')
+            print('❗ Sem dados para emitir')
+            p.press('esc', presses=4)
+            return False
+        
+    p.press('enter')
+    _escreve_relatorio_csv(f'{cod};{cnpj};{nome};Arquivo DIRF {ano} gerado', nome=andamento, end=';')
+    print('✔ Arquivo gerado')
+    
+    time.sleep(2)
+    mover_arquivo(empresa)
+    return True
+
+
+def salvar_pdf(empresa):
+    p.hotkey('ctrl', 'd')
+    
+    timer = 0
+    while not _find_img('procurar_pasta.png', conf=0.9):
+        time.sleep(1)
+        timer += 1
+        if timer > 30:
+            return False
+    
+    if not _find_img('cliente_c_selecionado.png', conf=0.9):
+        while not _find_img('cliente_c.png', conf=0.9):
+            _click_img('botao.png', conf=0.9)
+            time.sleep(3)
+        
+        _click_img('cliente_c.png', conf=0.9, timeout=1)
+        time.sleep(3)
+
+    while not _find_img('arquivos.png', conf=0.9):
+        time.sleep(1)
+
+    _click_img('arquivos.png', conf=0.9, timeout=1)
+    
+    p.hotkey('alt', 'o')
+    
+    while not _find_img('pdf_aberto.png', pasta='imgs_c', conf=0.9):
+        time.sleep(1)
+    
+    while _find_img('pdf_aberto.png', pasta='imgs_c', conf=0.9):
+        p.hotkey('alt', 'f4')
+        time.sleep(3)
+    
+    while _find_img('sera_finalizada.png', pasta='imgs_c', conf=0.9):
+        p.press('esc')
+
+    mover_relatorio(empresa)
+    return True
+
+
+def mover_arquivo(empresa):
+    cod, cnpj, nome = empresa
+
+    download_folder = "V:\\Setor Robô\\Scripts Python\\Domínio\\Gera Arquivo e Relatório DIRF"
+    folder = "V:\\Setor Robô\\Scripts Python\\Domínio\\Gera Arquivo e Relatório DIRF\\execução\\Arquivos"
+    guia = os.path.join(download_folder, f'DIRF{cod}.txt')
+    while os.path.exists(guia):
+        try:
+            shutil.move(guia, os.path.join(folder, f'DIRF{cod}.txt'))
+            time.sleep(2)
+        except:
+            pass
+
+
+def mover_relatorio(empresa):
+    cod, cnpj, nome = empresa
+
+    download_folder = "C:\\Arquivos"
+    folder = "V:\\Setor Robô\\Scripts Python\\Domínio\\Gera Arquivo e Relatório DIRF\\execução\\Relatórios"
+    guia = os.path.join(download_folder, 'DIRF.pdf')
+    while os.path.exists(guia):
+        try:
+            shutil.move(guia, os.path.join(folder, f'DIRF{cod} - {cnpj} - {nome}.pdf'))
+            time.sleep(2)
+        except:
+            pass
+
+    
 @_time_execution
 def run():
     ano = p.prompt(text='Qual ano base?', title='Script incrível', default='0000')

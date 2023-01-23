fcba1a4c5e862045b24e01ae5161ac789896460a
WilRocha97 <willrbr1997@gmail.com>
2023-01-23 16:37:18 -0300

    backup 23-01-2023

--- Domínio/Gera Arquivo e Relatório DIRF/gera_arquivo_relatorio_dirf.py
+++ Domínio/Gera Arquivo e Relatório DIRF/gera_arquivo_relatorio_dirf.py
@@ -7,7 +7,7 @@ from sys import path
 path.append(r'..\..\_comum')
 from pyautogui_comum import _find_img, _click_img, _wait_img
 from comum_comum import _indice, _time_execution, _escreve_relatorio_csv, e_dir, _open_lista_dados, _where_to_start
-from dominio_comum import _login
+from dominio_comum import _login, _salvar_pdf
 
 
 def dirf(empresa, ano, andamento):
@@ -42,9 +42,11 @@ def dirf(empresa, ano, andamento):
     
     arquivos_dirf(empresa, ano, andamento)
     
-    if not relatorio_dirf(empresa, ano, andamento):
-        return False
+    relatorio_dirf(empresa, ano, andamento)
     
+    p.press('esc', presses=4)
+    time.sleep(2)
+
     
 def relatorio_dirf(empresa, ano, andamento):
     # seleciona para gerar o relatório
@@ -99,14 +101,24 @@ def relatorio_dirf(empresa, ano, andamento):
     
     while not _find_img('relatorio_gerado.png', conf=0.9):
         time.sleep(1)
-        
-    salvar_pdf(empresa)
+
+        if _find_img('relatorio_gerado_2.png', conf=0.9):
+            _salvar_pdf()
+            mover_relatorio_2(empresa)
+            break
+
+        if _find_img('sem_dados_arquivo.png', conf=0.9):
+            p.press('enter')
+            _escreve_relatorio_csv('Sem dados para emitir', nome=andamento)
+            print('❗ Sem dados para emitir')
+            return False
+
+    if _find_img('relatorio_gerado.png', conf=0.9):
+        salvar_pdf(empresa)
     
     _escreve_relatorio_csv(f'Relatório DIRF {ano} gerado', nome=andamento)
     print('✔ Relatório gerado')
 
-    p.press('esc', presses=4)
-    time.sleep(2)
     return True
 
 
@@ -127,8 +139,9 @@ def arquivos_dirf(empresa, ano, andamento):
     
     # digita o caminho para salvar o arquivo
     os.makedirs('execução/Arquivos', exist_ok=True)
+    pyperclip.copy('')
     pyperclip.copy('V:\Setor Robô\Scripts Python\Domínio\Gera Arquivo e Relatório DIRF')
-    pyperclip.copy('V:\Setor Robô\Scripts Python\Domínio\Gera Arquivo e Relatório DIRF')
+    time.sleep(1)
     p.hotkey('ctrl', 'v')
     
     # abre a janela de outros dados
@@ -151,7 +164,6 @@ def arquivos_dirf(empresa, ano, andamento):
             p.press('enter')
             _escreve_relatorio_csv(f'{cod};{cnpj};{nome};Sem dados para emitir', nome=andamento, end=';')
             print('❗ Sem dados para emitir')
-            p.press('esc', presses=4)
             return False
         
     p.press('enter')
@@ -169,6 +181,12 @@ def salvar_pdf(empresa):
     timer = 0
     while not _find_img('procurar_pasta.png', conf=0.9):
         time.sleep(1)
+        if _find_img('salvar_em_pdf.png', pasta='imgs_c', conf=0.9):
+            p.press('esc')
+            _salvar_pdf()
+            mover_relatorio_3(empresa)
+            return True
+
         timer += 1
         if timer > 30:
             return False
@@ -190,6 +208,8 @@ def salvar_pdf(empresa):
     
     while not _find_img('pdf_aberto.png', pasta='imgs_c', conf=0.9):
         time.sleep(1)
+        if _find_img('adobe.png', pasta='imgs_c', conf=0.9):
+            _click_img('adobe.png', pasta='imgs_c', conf=0.9)
     
     while _find_img('pdf_aberto.png', pasta='imgs_c', conf=0.9):
         p.hotkey('alt', 'f4')
@@ -218,6 +238,9 @@ def mover_arquivo(empresa):
 
 def mover_relatorio(empresa):
     cod, cnpj, nome = empresa
+    cnpj = cnpj.replace('/', '').replace('.', '').replace('-', '')
+    nome = nome.replace('/', ' ')
+    os.makedirs('execução/Relatórios', exist_ok=True)
 
     download_folder = "C:\\Arquivos"
     folder = "V:\\Setor Robô\\Scripts Python\\Domínio\\Gera Arquivo e Relatório DIRF\\execução\\Relatórios"
@@ -229,7 +252,46 @@ def mover_relatorio(empresa):
         except:
             pass
 
-    
+
+def mover_relatorio_2(empresa):
+    cod, cnpj, nome = empresa
+    cnpj = cnpj.replace('/', '').replace('.', '').replace('-', '')
+    nome = nome.replace('/', ' ')
+    os.makedirs('execução/Relatórios', exist_ok=True)
+
+    folder = "V:\\Setor Robô\\Scripts Python\\Domínio\\Gera Arquivo e Relatório DIRF\\execução\\Relatórios"
+    guia = os.path.join('C:\\DIRF Escrita.pdf')
+    while not os.path.exists(guia):
+        time.sleep(1)
+        
+    while os.path.exists(guia):
+        try:
+            shutil.move(guia, os.path.join(folder, f'DIRF{cod} - {cnpj} - {nome}.pdf'))
+            time.sleep(2)
+        except:
+            pass
+
+
+
+def mover_relatorio_3(empresa):
+    cod, cnpj, nome = empresa
+    cnpj = cnpj.replace('/', '').replace('.', '').replace('-', '')
+    nome = nome.replace('/', ' ')
+    os.makedirs('execução/Relatórios', exist_ok=True)
+
+    folder = "V:\\Setor Robô\\Scripts Python\\Domínio\\Gera Arquivo e Relatório DIRF\\execução\\Relatórios"
+    guia = os.path.join('C:\\DIRF Folha.pdf')
+    while not os.path.exists(guia):
+        time.sleep(1)
+
+    while os.path.exists(guia):
+        try:
+            shutil.move(guia, os.path.join(folder, f'DIRF{cod} - {cnpj} - {nome}.pdf'))
+            time.sleep(2)
+        except:
+            pass
+
+
 @_time_execution
 def run():
     ano = p.prompt(text='Qual ano base?', title='Script incrível', default='0000')

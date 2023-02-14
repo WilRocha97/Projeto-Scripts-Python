732542eece5c54001f6be2c9bd3e43f5ce352032
WilRocha97 <willrbr1997@gmail.com>
2023-02-14 15:11:09 -0300

    backup 14-02-2023

--- SIEG/Download guias DCTF WEB/download_dctf_web_sieg.py
+++ SIEG/Download guias DCTF WEB/download_dctf_web_sieg.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pyautogui import prompt
-import os, time, re, csv
+import os, time, re, csv, shutil
 
 from sys import path
 path.append(r'..\..\_comum')
@@ -90,10 +90,6 @@ def procura_empresa(competencia, empresa, driver, options):
                 _escreve_relatorio_csv(f'{cnpj};{nome};Erro ao pesquisar empresa')
                 print('❗ Erro ao pesquisar empresa')
                 return driver
-    
-    # clica para abrir a barra de pesquisa
-    driver.find_element(by=By.ID, value='select2-ddlCompanyIris-container').click()
-    time.sleep(1)
 
     # clica para abrir a barra de pesquisa
     driver.find_element(by=By.CLASS_NAME, value='select2-search__field').send_keys(cnpj)
@@ -116,88 +112,160 @@ def procura_empresa(competencia, empresa, driver, options):
 
     # espera a lista de arquivos carregar, se não carregar tenta pesquisar novamente
     timer = 0
-    while not localiza_path(driver, '/html/body/form/div[5]/div[3]/div[1]/div/div[4]/div/table/tbody/tr[2]/td[9]/div/a[1]'):
+    while not localiza_path(driver, '/html/body/form/div[5]/div[3]/div/div/div[3]/div/table/tbody/tr[1]/td/div/span'):
         time.sleep(1)
         timer += 1
         if timer >= 60:
-            print('>>> Teantando novamente\n')
-            driver.close()
-            status, driver = _initialize_chrome(options)
-            driver = login_sieg(driver)
-            driver = sieg_iris(driver)
-            procura_empresa(competencia, empresa, driver, options)
-            return driver
-
+            try:
+                re.compile(r'class=\"\">(Nenhum item encontrado!)<').search(driver.page_source).group(1)
+                _escreve_relatorio_csv(f'{cnpj};{nome};Nenhuma guia de pagamento encontrado para esssa empresa')
+                print('❗ Nenhuma guia de pagamento encontrado para esssa empresa')
+                return driver
+            except:
+                print('>>> Teantando novamente\n')
+                driver.close()
+                status, driver = _initialize_chrome(options)
+                driver = login_sieg(driver)
+                driver = sieg_iris(driver)
+                procura_empresa(competencia, empresa, driver, options)
+                return driver
+    
     print('>>> Verificando donwload')
     try:
-        re.compile(r'>(' + competencia + ')<').search(driver.page_source).group(1)
+        # clica para expandir a lista de arquivos
+        driver.find_element(by=By.XPATH, value='/html/body/form/div[5]/div[3]/div[1]/div/div[3]/div/table/tbody/tr[1]/td/div/span/span').click()
+        time.sleep(2)
     except:
-        _escreve_relatorio_csv(f'{cnpj};{nome};Guia referênte ao mês {competencia} não disponível')
-        print(f'❗ Guia referênte ao mês {competencia} não disponível')
+        re.compile(r'class=\"\">(Nenhum item encontrado!)<').search(driver.page_source).group(1)
+        _escreve_relatorio_csv(f'{cnpj};{nome};Nenhuma guia de pagamento encontrado para esssa empresa')
+        print('❗ Nenhuma guia de pagamento encontrado para esssa empresa')
         return driver
     
-    try:
-        # clica em download
-        driver.find_element(by=By.XPATH, value='/html/body/form/div[5]/div[3]/div[1]/div/div[4]/div/table/tbody/tr[2]/td[9]/div/a[1]').click()
-        time.sleep(1)
-        # download dctf
-        driver.find_element(by=By.XPATH, value='/html/body/form/div[5]/div[3]/div[1]/div/div[4]/div/table/tbody/tr[2]/td[9]/div/ul/li[3]/a').click()
-        time.sleep(1)
-    
-        data = re.compile(r'td-background-dt hidden-md hidden-sm hidden-xs\">(.+)</td><td class=\"td-background-dt sorting_2\">(\d\d/\d\d\d\d)').search(driver.page_source).group(2)
-        data = data.replace('/', '_')
-        data2 = data.split('_')
-        
-        download_folder = "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\execução\\Guias"
-        guia = os.path.join(download_folder, cnpj + '_DCTFWEB_DarfOrDae_' + data + '.pdf')
-        guia2 = os.path.join(download_folder, cnpj + '_DCTFWEB_DarfOrDae_' + str(data2[1]) + '.pdf')
-        while not os.path.exists(guia):
-            if os.path.exists(guia2):
-                break
-            time.sleep(1)
-        
-        data = data.replace('_', '-')
-        _escreve_relatorio_csv(f'{cnpj};{nome};Guia salva {data}', nome='Download DCTF web')
-        
-        # deleta o andamento que já baixou a guia
-        file = "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\ignore\\Dados.csv"
-        with open(file, 'r', encoding='latin-1') as f:
-            dados = f.readlines()
-        
-        dados  = list(map(lambda x: tuple(x.replace('\n', '').split(';')), dados))
-    
-        f = open(file, 'w', encoding='latin-1')
-        f.write('')
-        f.close()
-        
-        for linha in dados:
-            if linha != empresa:
-                cnpj, nome = linha
-                f = open(file, 'a', encoding='latin-1')
-                f.write(f'{cnpj};{nome}\n')
-                f.close()
-            
-        print('✔ Download da guia concluído')
-    except:
-        _escreve_relatorio_csv(f'{cnpj};{nome};Não possuí guia para download')
-        print('❌ Não possuí guia para download')
+    # pega a lista de guias da competência desejada
+    guias = re.compile(r'</td><td class=\"td-background-dt sorting_2\">' + competencia + '</td><td class=\" td-background-dt\">.+</td><td class=\" td-background-dt\">R\$&nbsp;.+</td><td class=\" td-background-dt hidden-sm hidden-xs\">.+\n.+\n.+<a id=\"(.+)\" class=\"btn iris-') \
+        .findall(driver.page_source)
+
+    # verifica se existe alguma guia referênte a competência digitada
+    if not guias:
+        _escreve_relatorio_csv(f'{cnpj};{nome};Nenhuma guia de pagamento referênte a {competencia} encontrado para esssa empresa')
+        print(f'❗ Nenhuma guia de pagamento referênte a {competencia} encontrado para esssa empresa')
         return driver
+
+    contador = 0
+    erro = ''
+    sem_guia = ''
+    # faz o download dos comprovantes
+    for guia in guias:
+        download = True
+        while download:
+            driver, contador, erro, download = download_comprovante(contador, driver, cnpj, guia)
+        if erro == 'erro':
+            sem_guia = 'Existe uma guia com o botão de download dezabilitado'
+            print(f'❌ Existe uma guia com o botão de download desabilitado')
+            _escreve_relatorio_csv(f'{cnpj};{nome};{sem_guia}')
+            return driver
+
+    _escreve_relatorio_csv(f'{cnpj};{nome};Guia {competencia} baixada;{contador} Arquivos', nome='Download DCTF web')
+    
+    # deleta o andamento que já baixou a guia
+    file = "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\ignore\\Dados.csv"
+    with open(file, 'r', encoding='latin-1') as f:
+        dados = f.readlines()
+
+    dados = list(map(lambda x: tuple(x.replace('\n', '').split(';')), dados))
+
+    f = open(file, 'w', encoding='latin-1')
+    f.write('')
+    f.close()
+
+    for linha in dados:
+        if linha != empresa:
+            cnpj, nome = linha
+            f = open(file, 'a', encoding='latin-1')
+            f.write(f'{cnpj};{nome}\n')
+            f.close()
             
+    time.sleep(1)
     return driver
+    
+def download_comprovante(contador, driver, cnpj, guia):
+    if not click(driver, cnpj, guia):
+        return driver, contador, 'erro', False
+    
+    download_folder = "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\ignore\\Guias"
+    final_folder = "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\execução\\Guias"
+
+    print('>>> Aguardando o download da guia')
+    contador_3 = 0
+    while os.listdir(download_folder) == []:
+        if contador_3 > 20:
+            if not click(driver, cnpj, guia):
+                return driver, contador, 'erro', False
+            contador_3 = 0
+        time.sleep(1)
+        contador_3 += 1
+    
+    # caso exista algum arquivo com problema, tenta de novo o mesmo arquivo
+    for arquivo in os.listdir(download_folder):
+        if re.compile(r'.tmp').search(arquivo):
+            os.remove(os.path.join(download_folder, arquivo))
+            return driver, contador, 'ok', True
+        
+        while re.compile(r'crdownload').search(arquivo):
+            print('>>> Aguardando download...')
+            time.sleep(3)
+            for arq in os.listdir(download_folder):
+                arquivo = arq
+        
+        else:
+            while not os.path.isfile(os.path.join(final_folder, arquivo)):
+                print('>>> Movendo arquivo')
+                shutil.move(os.path.join(download_folder, arquivo), os.path.join(final_folder, arquivo))
+                time.sleep(2)
+                        
+            print(f'✔ {arquivo}')
+            contador += 1
+            
+            for arquivo in os.listdir(download_folder):
+                os.remove(os.path.join(download_folder, arquivo))
+    
+    return driver, contador, 'ok', False
+
+
+def click(driver, cnpj, guia):
+    codigo_id = guia.split('-')[1]
+    contador_2 = 0
+    clicou = 'não'
+    while clicou == 'não':
+        try:
+            driver.find_element(by=By.ID, value=guia).click()
+            time.sleep(0.5)
+            nome = "DownloadDCTFWeb('" + codigo_id + "', '" + cnpj + "', 'DarfOrDae')"
+            driver.find_element(by=By.CSS_SELECTOR, value='a[onclick^="' + nome + '"]').click()
+            clicou = 'sim'
+            print('>>> Baixando guia')
+        except:
+            contador_2 += 1
+            clicou = 'não'
+        if contador_2 > 5:
+            return False
+    
+    return True
 
 
 @_time_execution
 def run():
     competencia = prompt(text='Qual competência referênte?', title='Script incrível', default='00/0000')
     os.makedirs('execução/Guias', exist_ok=True)
+    os.makedirs('ignore/Guias', exist_ok=True)
     
     # opções para fazer com que o chome trabalhe em segundo plano (opcional)
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
-    # options.add_argument('--window-size=1920,1080')
+    # options.add_argument('--window-size=1366,768')
     options.add_argument("--start-maximized")
     options.add_experimental_option('prefs', {
-        "download.default_directory": "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\execução\\Guias",  # Change default directory for downloads
+        "download.default_directory": "V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\ignore\\Guias",  # Change default directory for downloads
         "download.prompt_for_download": False,  # To auto download the file
         "download.directory_upgrade": True,
         "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
@@ -233,11 +301,11 @@ def run():
             _indice(count, total_empresas, empresa)
             erro = 'sim'
             while erro == 'sim':
-                try:
-                    driver = sieg_iris(driver)
-                    driver = procura_empresa(competencia, empresa, driver, options)
-                    erro = 'não'
-                except:
+                '''try:'''
+                driver = sieg_iris(driver)
+                driver = procura_empresa(competencia, empresa, driver, options)
+                erro = 'não'
+                '''except:
                     try:
                         erro = 'sim'
                         driver.close()
@@ -246,7 +314,7 @@ def run():
                     except:
                         erro = 'sim'
                         status, driver = _initialize_chrome(options)
-                        driver = login_sieg(driver)
+                        driver = login_sieg(driver)'''
 
         contador += 1
         os.remove("V:\\Setor Robô\\Scripts Python\\SIEG\\Download guias DCTF WEB\\execução\\resumo.csv")

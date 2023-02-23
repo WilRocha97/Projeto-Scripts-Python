306a0ece786f16e90e4f4f320b8a7bc62df2c6c0
WilRocha97 <willrbr1997@gmail.com>
2023-02-23 13:34:20 -0300

    backup 23-02-2023

--- Geral/Pega info Encargos IRRF Domínio/separa_e_pega_info_de_pdf_encargos_irrf.py
+++ Geral/Pega info Encargos IRRF Domínio/separa_e_pega_info_de_pdf_encargos_irrf.py
@@ -32,8 +32,8 @@ def cria_pdf(pasta_final, page, periodo, nome, cnpj, codigo, prev_texto_nome, pr
         text = f'Encargos IRRF Domínio Web - {cnpj_pdf} - {nome_pdf}.pdf'
         
         # Define o caminho para salvar o pdf
-        os.makedirs(os.path.join(pasta_final, f'Relatorios {data}'), exist_ok=True)
-        arquivo = os.path.join(pasta_final, f'Relatorios {data}', text)
+        os.makedirs(os.path.join(pasta_final, f'Relatorios'), exist_ok=True)
+        arquivo = os.path.join(pasta_final, f'Relatorios', text)
         
         # Define a página inicial e a final
         new_pdf.insert_pdf(pdf, from_page=pagina1, to_page=pagina2)
@@ -65,54 +65,56 @@ def separa():
         # para cada página do pdf
         for page in pdf:
             andamento = f'Pagina = {str(page.number + 1)}'
-            try:
-                # Pega o texto da pagina
-                textinho = page.get_text('text', flags=1 + 2 + 8)
+            '''try:'''
+            # Pega o texto da pagina
+            textinho = page.get_text('text', flags=1 + 2 + 8)
 
-                # Procura o nome da empresa no texto do pdf
-                empresa = re.compile(r'(\d+) - (.+)\n(.+)').search(textinho)
-                if not empresa:
-                    continue
-
-                valores = re.compile(r'(\d\d\d\d)\n(.+,\d+)\n(.+,\d+)\n(.+,\d+)\n(.+,\d+)\n(\d\d/\d\d\d\d)\n(.+)').search(textinho)
-                if valores:
-                    periodo = valores.group(6)
-                    
-                codigo = empresa.group(1)
-                nome = empresa.group(2)
-                cnpj = empresa.group(3)
-                
-                # Se estiver na primeira página, guarda as informações
-                if page.number == 0:
-                    prev_pagina, prev_texto_periodo, prev_texto_nome, prev_texto_cnpj, prev_texto_cod = guarda_info(page, periodo, nome, cnpj, codigo)
-                    continue
-                
-                # Se o nome da página atual for igual ao da anterior, soma um indice de páginas
-                if cnpj == prev_texto_cnpj:
-                    paginas += 1
-                    # Guarda as informações da página atual
-                    prev_pagina, prev_texto_periodo, prev_texto_nome, prev_texto_cnpj, prev_texto_cod = guarda_info(page, periodo, nome, cnpj, codigo)
-                    continue
-                
-                # Se for diferente ele separa a página
-                else:
-                    if paginas > 0:
-                        # define qual é a primeira página e o nome da empresa
-                        paginainicial = prev_pagina - paginas
-                        andamento = f'Paginas = {str(paginainicial + 1)} até {str(prev_pagina + 1)}'
-                        prev_pagina, prev_texto_periodo ,prev_texto_nome, prev_texto_cnpj, prev_texto_cod = cria_pdf(pasta_final, page, periodo, nome, cnpj, codigo,
-                                                                             prev_texto_nome, prev_texto_cnpj, pdf,
-                                                                             paginainicial, prev_pagina)
-                        paginas = 0
-                    # Se for uma página entra a qui
-                    elif paginas == 0:
-                        andamento = f'Pagina = {str(prev_pagina + 1)}'
-                        prev_pagina, prev_texto_periodo, prev_texto_nome, prev_texto_cnpj, prev_texto_cod = cria_pdf(pasta_final, page, periodo, nome, cnpj, codigo,
-                                                                             prev_texto_nome, prev_texto_cnpj, pdf,
-                                                                             prev_pagina, prev_pagina)
-            except:
-                _escreve_relatorio_csv(andamento, nome='Erro')
+            # Procura o nome da empresa no texto do pdf
+            empresa = re.compile(r'(\d+) - (.+)\n(.+)').search(textinho)
+            if not empresa:
                 continue
+
+            valores = re.compile(r'(\d\d\d\d)\n(.+,\d+)\n(.+,\d+)\n(.+,\d+)\n(.+,\d+)\n(\d\d/\d\d\d\d)\n(.+)').search(textinho)
+            if valores:
+                periodo = valores.group(5)
+            else:
+                periodo = 'Não consta'
+                
+            codigo = empresa.group(1)
+            nome = empresa.group(2)
+            cnpj = empresa.group(3)
+            
+            # Se estiver na primeira página, guarda as informações
+            if page.number == 0:
+                prev_pagina, prev_texto_periodo, prev_texto_nome, prev_texto_cnpj, prev_texto_cod = guarda_info(page, periodo, nome, cnpj, codigo)
+                continue
+            
+            # Se o nome da página atual for igual ao da anterior, soma um indice de páginas
+            if cnpj == prev_texto_cnpj:
+                paginas += 1
+                # Guarda as informações da página atual
+                prev_pagina, prev_texto_periodo, prev_texto_nome, prev_texto_cnpj, prev_texto_cod = guarda_info(page, periodo, nome, cnpj, codigo)
+                continue
+            
+            # Se for diferente ele separa a página
+            else:
+                if paginas > 0:
+                    # define qual é a primeira página e o nome da empresa
+                    paginainicial = prev_pagina - paginas
+                    andamento = f'Paginas = {str(paginainicial + 1)} até {str(prev_pagina + 1)}'
+                    prev_pagina, prev_texto_periodo ,prev_texto_nome, prev_texto_cnpj, prev_texto_cod = cria_pdf(pasta_final, page, periodo, nome, cnpj, codigo,
+                                                                         prev_texto_nome, prev_texto_cnpj, pdf,
+                                                                         paginainicial, prev_pagina)
+                    paginas = 0
+                # Se for uma página entra a qui
+                elif paginas == 0:
+                    andamento = f'Pagina = {str(prev_pagina + 1)}'
+                    prev_pagina, prev_texto_periodo, prev_texto_nome, prev_texto_cnpj, prev_texto_cod = cria_pdf(pasta_final, page, periodo, nome, cnpj, codigo,
+                                                                         prev_texto_nome, prev_texto_cnpj, pdf,
+                                                                         prev_pagina, prev_pagina)
+            '''except:
+                _escreve_relatorio_csv(andamento, nome='Erro')
+                continue'''
         
         # Faz o mesmo dos dois de cima apenas para a(as) ultima(as) página(as)
         try:
@@ -151,7 +153,7 @@ def analiza(file):
                     # se existir uma empresa diferente sem código anterior a empresa atual, anota na planilha
                     if sem_codigo_receita == 'sim':
                         if codigo != codigo_anterior:
-                            _escreve_relatorio_csv(f"{codigo_anterior};{cnpj_anterior};{nome_anterior};Não consta;{periodo_anterior};Não consta;{valor_anterior};Não consta;Não consta;Não consta", nome=f'Encargos de IRRF - {periodo.replace("/", "-")}')
+                            _escreve_relatorio_csv(f"{codigo_anterior};{cnpj_anterior};{nome_anterior};Não consta;{periodo_anterior};Não consta;{valor_anterior};Não consta;Não consta;Não consta")
     
                     try:
                         valor_anterior = re.compile(r'Total Geral:\n(.+,\d+)').search(textinho).group(1)
@@ -174,11 +176,11 @@ def analiza(file):
                         codigo = empresa.group(1)
                         nome = empresa.group(2)
                         cnpj = empresa.group(3)
-
+                        
                         # se existir uma empresa diferente sem código anterior a empresa atual, anota na planilha
                         if sem_codigo_receita == 'sim':
                             if codigo != codigo_anterior:
-                                _escreve_relatorio_csv(f"{codigo_anterior};{cnpj_anterior};{nome_anterior};Não consta;{periodo_anterior};Não consta;{valor_anterior};Não consta;Não consta;Não consta", nome=f'Encargos de IRRF - {periodo.replace("/", "-")}')
+                                _escreve_relatorio_csv(f"{codigo_anterior};{cnpj_anterior};{nome_anterior};Não consta;{periodo_anterior};Não consta;{valor_anterior};Não consta;Não consta;Não consta")
                                 sem_codigo_receita = 'não'
                                 
                         periodicidade = valor[6]
@@ -191,7 +193,7 @@ def analiza(file):
                         
                         # print(f'{codigo} - {cnpj} - {nome} - {valor}')
                         
-                        _escreve_relatorio_csv(f"{codigo};{cnpj};{nome};{periodicidade};{periodo};{codigo_receita};{valor_recolher};{valor_compensar};{valor_pagar};{valor_acumular}", nome=f'Encargos de IRRF - {periodo.replace("/", "-")}')
+                        _escreve_relatorio_csv(f"{codigo};{cnpj};{nome};{periodicidade};{periodo};{codigo_receita};{valor_recolher};{valor_compensar};{valor_pagar};{valor_acumular}")
                         sem_codigo_receita = 'não'
                             
             except():
@@ -206,7 +208,7 @@ if __name__ == '__main__':
 
     inicio = datetime.now()
     periodo = analiza(file)
-    _escreve_header_csv('CÓDIGO;CNPJ;NOME;PERIODICIDADE;PERÍODO;CÓDIGO RECOLHIMENTO;VALOR A RECOLHER;VALOR A COMPENSAR;VALOR A PAGAR;VALOR A ACUMULAR', nome=f'Encargos de IRRF - {periodo.replace("/", "-")}')
+    _escreve_header_csv('CÓDIGO;CNPJ;NOME;PERIODICIDADE;PERÍODO;CÓDIGO RECOLHIMENTO;VALOR A RECOLHER;VALOR A COMPENSAR;VALOR A PAGAR;VALOR A ACUMULAR')
     print(datetime.now() - inicio)
     
     messagebox.showinfo(title=None, message='Finalizado com sucesso')

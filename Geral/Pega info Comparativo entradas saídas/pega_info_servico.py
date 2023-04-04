ef3defdc225c6dc5497395cb8a707e38170837af
WilRocha97 <willrbr1997@gmail.com>
2023-04-04 15:04:35 -0300

    backup 04-03-2023

--- Geral/Pega info Comparativo entradas saídas/pega_info_servico.py
+++ Geral/Pega info Comparativo entradas saídas/pega_info_servico.py
@@ -5,32 +5,39 @@ from datetime import datetime
 
 from sys import path
 path.append(r'..\..\_comum')
-from comum_comum import ask_for_file
+from comum_comum import ask_for_dir
 from comum_comum import _escreve_relatorio_csv, _escreve_header_csv
 
 
 def analiza():
-    arq = ask_for_file(filetypes=[('PDF files', '*.pdf *')])
-    # Abrir o pdf
-    with fitz.open(arq) as pdf:
-        # Para cada página do pdf, se for a segunda página o script ignora
-        for page in pdf:
-            try:
-                # Pega o texto da pagina
-                textinho = page.get_text('text', flags=1 + 2 + 8)
-
-                total_servico = re.compile(r'Totais\n.+,.+\n(.+,.+)').search(textinho).group(1)
-                nome = re.compile(r'Empresa:\n(.+)').search(textinho).group(1)
-                cnpj = re.compile(r'CNPJ:\n(.+)').search(textinho).group(1)
-                
-                periodo = re.compile(r'Período:\n(.+\n.+)').search(textinho).group(1)
-                periodo = periodo.replace('/', '-').replace('\na', ' até ')
-                
-                _escreve_relatorio_csv(f"{cnpj};{nome};{periodo};{total_servico}", nome=f'Relatório de Faturamento Serviço - {periodo}')
-        
-            except():
-                print(textinho)
-                print('ERRO')
+    documentos = ask_for_dir()
+    # Analiza cada pdf que estiver na pasta
+    for arq in os.listdir(documentos):
+        if not arq.endswith('.pdf'):
+            continue
+        print(f'\nArquivo: {arq}')
+        _escreve_relatorio_csv(f"{arq}")
+        # Abrir o pdf
+        arq = os.path.join(documentos, arq)
+        with fitz.open(arq) as pdf:
+            # Para cada página do pdf, se for a segunda página o script ignora
+            for page in pdf:
+                try:
+                    # Pega o texto da pagina
+                    textinho = page.get_text('text', flags=1 + 2 + 8)
+    
+                    total_servico = re.compile(r'Totais\n.+,.+\n(.+,.+)').search(textinho).group(1)
+                    nome = re.compile(r'Empresa:\n(.+)').search(textinho).group(1)
+                    cnpj = re.compile(r'CNPJ:\n(.+)').search(textinho).group(1)
+                    
+                    periodo = re.compile(r'Período:\n(.+\n.+)').search(textinho).group(1)
+                    periodo = periodo.replace('/', '-').replace('\na', ' até ')
+                    
+                    _escreve_relatorio_csv(f"{cnpj};{nome};{periodo};{total_servico}", nome=f'Relatório de Faturamento Serviço - {periodo}')
+            
+                except():
+                    print(textinho)
+                    print('ERRO')
 
     return periodo
 

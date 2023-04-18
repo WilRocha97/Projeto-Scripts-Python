a0ddedcd20b811149a9fe0b24b57d75592fbb503
WilRocha97 <willrbr1997@gmail.com>
2023-04-18 13:14:15 -0300

    backup 18-04-2023

---
+++ Sicalc/Gerador de guias de DARF WEB/gerador_guias_darf_irf.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+import pyautogui as p
+import os, time, shutil, re
+
+from sys import path
+path.append(r'..\..\_comum')
+from comum_comum import _time_execution, _escreve_relatorio_csv, _open_lista_dados, _where_to_start, _indice, _headers, e_dir
+from pyautogui_comum import _click_img, _wait_img, _find_img
+from captcha_comum import _solve_hcaptcha
+
+
+def login_sicalc(empresa):
+    cnpj, nome, nota, valor, cod = empresa
+    p.hotkey('win', 'm')
+    
+    # Abrir o site
+    if _find_img('Chrome.png', conf=0.99):
+        pass
+    elif _find_img('ChromeAberto.png', conf=0.99):
+        _click_img('ChromeAberto.png', conf=0.99)
+    else:
+        time.sleep(0.5)
+        _click_img('ChromeAtalho.png', conf=0.9, clicks=2)
+        while not _find_img('Google.png', conf=0.9, ):
+            time.sleep(5)
+            p.moveTo(1163, 377)
+            p.click()
+    
+    link = 'https://sicalc.receita.economia.gov.br/sicalc/rapido/contribuinte'
+    
+    _click_img('Maxi.png', conf=0.9, timeout=1)
+    p.click(1150, 51)
+    time.sleep(1)
+    p.write(link.lower())
+    time.sleep(1)
+    p.press('enter')
+    time.sleep(3)
+    
+    # selecionar o checkbox do captcha
+    p.press('pgDn')
+    _click_img('checkbox.png', conf=0.95)
+    p.moveTo(5, 5)
+    _wait_img('check.png', conf=0.9, timeout=1)
+    
+    _click_img('possoa_juridica.png', conf=0.95)
+    time.sleep(1)
+    p.press('tab')
+    time.sleep(1)
+    p.write(cnpj)
+    time.sleep(1)
+    p.press('tab', presses=5, interval=0.1)
+    time.sleep(1)
+    p.press('enter')
+    
+    return True
+    
+
+def gerar(empresa, apuracao, tipo):
+    cnpj, nome, nota, valor, cod = empresa
+
+    # esperar o menu referente a guia aparecer
+    timer = 0
+    while not _find_img('menu.png', conf=0.9):
+        time.sleep(1)
+        timer += 1
+        if timer >= 10:
+            return False
+    
+    print('>>> Inserindo informações da guia')
+    
+    # insere observação
+    nota.split(',')
+    p.write(f'Referente à NF: {nota} - R. POSTAL')
+    time.sleep(1)
+    
+    # inserir o código da receita referente a guia
+    p.press('pgDn')
+    time.sleep(1)
+    p.press('tab')
+    time.sleep(1)
+    p.write(cod)
+    time.sleep(1)
+    
+    # confirmar a seleção
+    p.press(['down', 'enter'], interval=1)
+    time.sleep(1)
+    p.moveTo(106, 375)
+    p.click()
+    time.sleep(1)
+
+    # descer a visualização da página
+    timer = 0
+    while not _find_img('apuracao.png', conf=0.95):
+        p.press('pgDn')
+        time.sleep(1)
+        timer += 1
+        if timer >= 20:
+            return False
+        
+    # clicar no campo e inserir a data de apuração
+    _click_img('apuracao.png', conf=0.95)
+    time.sleep(1)
+    p.write(apuracao)
+    time.sleep(1)
+
+    # descer a visualização da página
+    while not _find_img('valor.png', conf=0.95):
+        p.press('pgDn')
+        time.sleep(1)
+        
+    # clicar no campo e inserir o valor
+    _click_img('valor.png', conf=0.95, clicks=2)
+    time.sleep(1)
+    p.write(valor)
+
+    p.press('tab')
+    time.sleep(1)
+
+    # espera guia ser calculada
+    timer = 0
+    while not _find_img('checkbox_guia.png', conf=0.95):
+        _click_img('calcular.png', conf=0.95)
+        # descer a visualização da página
+        p.press('pgDn')
+        time.sleep(1)
+        timer += 1
+        if timer >= 20:
+            return False
+        
+    print('>>> Guia calculada')
+    time.sleep(43)
+    
+    # marca a guia que será gerada
+    erro = 'sim'
+    timer = 0
+    while erro == 'sim':
+        try:
+            _click_img('checkbox_guia.png', conf=0.95)
+            time.sleep(1)
+            erro = 'não'
+        except:
+            _click_img('calcular.png', conf=0.95)
+            erro = 'sim'
+        
+        time.sleep(1)
+        timer+= 1
+        if timer >= 15:
+            return False
+
+    print('>>> Gerando guia')
+    _click_img('emitir_darf.png', conf=0.95)
+    return True
+
+
+def salvar_guia(empresa, vencimento):
+    cnpj, nome, nota, valor, cod = empresa
+    _wait_img('SalvarComo.png', conf=0.9, timeout=-1)
+    # exemplo: NOMEDA EMPRESA - 00000000000 - DARF IRRF 170806 02-2023 - venc. 20-03-2023.pdf
+    p.write(f'{nome.replace("/", " ")} - {cnpj} - {tipo} {cod} {apuracao.replace("/", "-")} - venc. {vencimento.replace("/", "-")}.pdf')
+    time.sleep(0.5)
+    
+    # Selecionar local
+    p.press('tab', presses=6)
+    time.sleep(0.5)
+    p.press('enter')
+    time.sleep(0.5)
+    pyperclip.copy('V:\Setor Robô\Scripts Python\Sicalc\Gerador de guias de DARF WEB\{}\{}'.format(e_dir, 'Guias'))
+    p.hotkey('ctrl', 'v')
+    time.sleep(0.5)
+    p.press('enter')
+    time.sleep(0.5)
+    p.hotkey('alt', 'l')
+    time.sleep(1)
+    if _find_img('Substituir.png'):
+        p.press('s')
+    time.sleep(1)
+    p.hotkey('ctrl', 'w')
+    
+    
+@_time_execution
+def run():
+    os.makedirs('execução/Guias', exist_ok=True)
+    # p.mouseInfo()
+    tipo = p.confirm(title='Script incrível', text='Qual tipo da guia?', buttons=('DARF IRRF', 'DARF DP'))
+    apuracao = p.prompt(title='Script incrível', text='Qual o período de apuração?', default='00/0000')
+    vencimento = p.prompt(title='Script incrível', text='Qual o vencimento das guias?', default='00/0000')
+    
+    # abrir a planilha de dados
+    empresas = _open_lista_dados()
+    if not empresas:
+        return False
+
+    # configurar um indice para a planilha de dados
+    index = _where_to_start(tuple(i[0] for i in empresas))
+    if index is None:
+        return False
+
+    p.hotkey('win', 'm')
+    total_empresas = empresas[index:]
+    for count, empresa in enumerate(empresas[index:], start=1):
+
+        # configurar o indice para localizar em qual empresa está
+        _indice(count, total_empresas, empresa)
+        
+       # erro = 'sim'
+        # while erro == 'sim':
+            # try:
+        # fazer login do SICALC
+        if not login_sicalc(empresa):
+            driver.close()
+            erro = 'sim'
+        else:
+            # gerar a guia de DCTF
+            if not gerar(empresa, str(apuracao), tipo):
+                erro = 'sim'
+            else:
+                salvar_guia(empresa, vencimento)
+                print('✔ Guia gerada')
+                _escreve_relatorio_csv('{};{};{};{};Guia gerada'.format(cnpj, nome, valor, cod), nome=f'Resumo gerador {tipo}')
+                erro = 'nao'
+        '''except:
+            try:
+                p.hotkey('alt', 'f4')
+                erro = 'sim'
+            except:
+                erro = 'sim'''
+
+        p.hotkey('alt', 'f4')
+
+if __name__ == '__main__':
+    run()

51f2c8ae50fc1468ccd4c846db95d850eaf23e21
WilRocha97 <willrbr1997@gmail.com>
2023-05-16 11:26:51 -0300

    backup 16-05-2023

--- Sindicatos/Boletos Sindicatos/modulos/guias_sindpd.py
+++ Sindicatos/Boletos Sindicatos/modulos/guias_sindpd.py
@@ -39,35 +39,55 @@ def login(driver, cnpj, senha):
             alert.accept()
         except:
             pass
+        
+    print('>>> Verificando boletos')
     
     avisos = re.compile('ATENÇÃO! (.+).<br> ').search(driver.page_source)
     if avisos:
-        avisos = avisos.replace('<br>', '')
         avisos = avisos.group(1)
+        avisos = avisos.replace('<br>', '')
         return driver, avisos
     
-    print('>>> Verificando boletos')
     return driver, ''
 
-def gera_boleto(driver, valor):
+def gera_boleto(driver, cnpj, valor, data, funcionarios, responsavel, email):
+    data = data.split('-')
+    data = f'{data[1]}/{data[0]}'
+    # clica em contribuição assistencial
+    driver.find_element(by=By.ID, value='contrib2').click()
+    
+    while not _find_by_path('/html/body/div[4]/b[2]/font/div[2]/table', driver):
+        time.sleep(1)
+    
+    print(driver.page_source)
     _escreve_relatorio_csv(f'{cnpj};{valor};Não tem nada aqui', nome='Boletos Sindicato')
     return driver
 
 
-def run(cnpj, valor, usuario, senha, funcionarios):
+def run(empresa):
+    # define as variáveis que serão usadas
+    cnpj = empresa[1]
+    valor = empresa[2]
+    data = empresa[5]
+    senha = empresa[7]
+    funcionarios = empresa[8]
+    responsavel = empresa[9]
+    email = empresa[10]
+    
     print('28 - SINDPD - Sindicato dos Trabalhadores em Processamento de Dados e Tecnologia da Informação do Estado de São Paulo')
+    
     options = webdriver.ChromeOptions()
     #options.add_argument('--headless')
     #options.add_argument('--window-size=1920,1080')
     options.add_argument("--start-maximized")
-    
+
     status, driver = _initialize_chrome(options)
     
+    # faz login na empresa
     driver, avisos = login(driver, cnpj, senha)
-    if not avisos:
-        driver = gera_boleto(driver, valor)
-        driver.close()
-        return '✔ Boleto gerado'
-    else:
-        driver.close()
-        return f'❌ {avisos}'
+    
+    # gera os boletos
+    driver = gera_boleto(driver, cnpj, valor, data, funcionarios, responsavel, email)
+    driver.close()
+    return f'✔ Boleto gerado - {avisos}'
+

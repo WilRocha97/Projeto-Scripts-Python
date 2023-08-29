d76e3d80f5da100f8b2d62014f3c869052364556
WilRocha97 <willrbr1997@gmail.com>
2023-08-29 17:19:11 -0300

    backup 29-08-2023

--- Sindicatos/Boletos Sindicatos/modulos/guias_sindesporte.py
+++ Sindicatos/Boletos Sindicatos/modulos/guias_sindesporte.py
@@ -6,22 +6,58 @@ from selenium.webdriver.common.by import By
 from sys import path
 
 path.append(r'..\..\_comum')
-from chrome_comum import _initialize_chrome, _find_by_id, _find_by_path
+from chrome_comum import _initialize_chrome, _find_by_id, _find_by_path, _send_input
 from comum_comum import _escreve_relatorio_csv
 from pyautogui_comum import _find_img, _click_img, _wait_img
 
 
 def login(driver, cnpj):
-    # entra no site
-    driver.get('http://sisnet.sindesporte.com.br/sisnet40/negocial.aspx')
     print('>>> Acessando o site')
+    # entra no site
+    timer = 0
+    while not _find_by_id('TextBoxCNPJ', driver):
+        # abre o site da consulta e caso de erro é porque o site demorou pra responder,
+        # nesse caso retorna um erro para tentar novamente
+        try:
+            driver.get('http://sisnet.sindesporte.com.br/sisnet40/negocial.aspx')
+        except:
+            return driver, False
+        time.sleep(1)
+        timer += 0
+        if timer > 120:
+            return driver, False
+    
+    _send_input('TextBoxCNPJ', cnpj, driver)
     time.sleep(1)
+    
+    # aperta ok para entrar
+    driver.find_element(by=By.ID, value='ButtonOk').click()
 
-    return driver, ''
+    return driver, True
 
-def gera_boleto(driver, cnpj, valor, funcionarios):
+def gera_boleto(driver, valor, funcionarios):
     print('>>> Verificando boletos')
-
+    # aguarda o botão de nova guia aparecer
+    while not _find_by_id('ButtonNovoBoleto', driver):
+        time.sleep(1)
+    
+    # aperta botão de nova guia
+    driver.find_element(by=By.ID, value='ButtonNovoBoleto').click()
+    
+    # aguarda o campo de quantidade de funcionários
+    while not _find_by_id('GridViewGuias_TextBoxEmpregados_0', driver):
+        time.sleep(1)
+    
+    # insere o número de funcionarios
+    _send_input('GridViewGuias_TextBoxEmpregados_0', funcionarios, driver)
+    time.sleep(1)
+    # insere o valor do boleto
+    _send_input('GridViewGuias_TextBoxValor_0', valor, driver)
+    time.sleep(1)
+    
+    # aperta botão de gerar o boleto
+    driver.find_element(by=By.ID, value='ButtonGerarGuia1').click()
+    time.sleep(33)
     
     return driver, resultados
 
@@ -42,11 +78,11 @@ def run(empresa):
     
     # faz login na empresa
     driver, resultados = login(driver, cnpj)
-    if not driver:
+    if not resultados:
         driver.close()
         return f'❌ Erro no login - {resultados}'
     
     # gera os boletos
-    driver, resultados = gera_boleto(driver, cnpj, valor, funcionarios)
+    driver, resultados = gera_boleto(driver, valor, funcionarios)
     driver.close()
     return f'✔ Boleto gerado - {resultados}'

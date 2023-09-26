9efc9e7137c632eaa0f306fa2e791ce472b140a4
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2023-09-26 07:59:34 -0300

    backup 26-09-2023

--- Esocial/Consulta Qualificação Cadastral/consulta_qualificacao _cadastral.py
+++ Esocial/Consulta Qualificação Cadastral/consulta_qualificacao _cadastral.py
@@ -182,17 +182,20 @@ def run():
 
         while True:
             # iniciar o driver do chrome
-            status, driver = _initialize_chrome(options)
+            try:
+                status, driver = _initialize_chrome(options)
             
-            # coloca um timeout de 60 segundos para que o robô não fique esperando eternamente caso o site não carregue
-            driver.set_page_load_timeout(60)
-            
-            # faz login no site
-            driver, resultado = login(driver, nome, cpf, pis, data_nasc)
-            # se não der erro no login, sai do while e realiza a consulta
-            if resultado != 'erro':
-                break
-            driver.close()
+                # coloca um timeout de 60 segundos para que o robô não fique esperando eternamente caso o site não carregue
+                driver.set_page_load_timeout(60)
+                
+                # faz login no site
+                driver, resultado = login(driver, nome, cpf, pis, data_nasc)
+                # se não der erro no login, sai do while e realiza a consulta
+                if resultado != 'erro':
+                    break
+                driver.close()
+            except:
+                pass
         
         if resultado == 'ok':
             driver, resultado = consulta(driver)
